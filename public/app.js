7cf54e40246e69ae2307c31e6ed06ddea60850cf
Skufu <agpfrancisco1@gmail.com>
2025-08-31 21:32:02 +0800

    comit

---
+++ public/app.js
@@ -0,0 +1,16 @@
+import { API } from "./services/API.js";
+import { HomePage } from "./components/HomePage.js";
+
+window.addEventListener("DOMContentLoaded", event => {
+    document.querySelector("main").appendChild(new HomePage());
+});
+
+window.app = {
+    search: (event) => {
+        event.preventDefault();
+        const q = document.querySelector("input[type=search]").value;
+        //TODO: Implement the search
+        
+    },
+    api: API
+}
